e9bb0108ef8791dca41858e9cbc7a95ebaeebdd3
Axliddin <axliddins34@gmail.com>
2025-01-29 12:35:34 +0500

    as

--- Quiz/settings.py
+++ Quiz/settings.py
@@ -117,10 +117,12 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
+import os
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 
-
+# Set STATIC_ROOT to the desired directory for static files
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Rest Framework settings
 
